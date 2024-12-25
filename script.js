71f5922d41512fbcd5003ef55a70ce07c307f573
thehomienova <thehomienova@gmail.com>
2024-12-24 17:33:54 -0800

    Add base files

---
+++ script.js
@@ -0,0 +1,23 @@
+function add (x, y) {
+    return x + y;
+}
+
+function subtract (x,y) {
+    return x-y;
+}
+
+function multiply (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    if (y === 0) {
+        return "cant divide by zero gang"; 
+    }
+
+    return x / y;
+}
+
+function operate(x, y, operartor) {
+    
+}
